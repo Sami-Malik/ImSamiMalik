fddccb004cf5162417a0ff7ffdbad015da0db6e1
Sami Malik <42075879+Sami-Malik@users.noreply.github.com>
2020-09-12 15:58:45 +0500

    added react-snap

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Loader from "./Loader";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+    ReactDOM.hydrate(<App />, rootElement);
+} else {
+    ReactDOM.render(<App />, rootElement);
+}
 ReactDOM.render(<Loader />, document.getElementById("loader_container"));
